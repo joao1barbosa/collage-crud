18c36cc780085ee87697fb770bb2b3c0d1ed60ef
joao1barbosa <joao1.barbosa@outlook.com>
2025-05-14 21:46:34 -0300

    fix: SQLs scripts

--- initdb/04_seed_users.sql
+++ initdb/04_seed_users.sql
@@ -1,5 +1,5 @@
-INSERT INTO users (username, password, role, student_registration)
+INSERT INTO "user" (username, password, role, student_registration)
 VALUES ('usuario1', 'senha123', 'USER', 202500001);
 
-INSERT INTO users (username, password, role)
+INSERT INTO "user" (username, password, role)
 VALUES ('admin', 'admin123', 'ADMIN');
